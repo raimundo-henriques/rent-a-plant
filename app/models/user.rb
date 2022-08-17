5961b28d1c468aa7a6957e42e328afd5a6e8c8e0
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-17 19:21:14 +0100

    TA prep: seeded plants

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :plants
+  has_many :plants, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Erasing everything..."
+
+User.destroy_all
+
+puts "Creating user..."
+
+user = User.create!(email: "abcd@example.com", password: "123456", first_name: "John", last_name:"Doe")
+puts "Created #{user.first_name} #{user.last_name}"
+
+puts "Fetching plants..."
+
+plants_url = "https://houseplantapi.herokuapp.com/all"
+plants_serialized = URI.open(plants_url).read
+plants_json = JSON.parse(plants_serialized)
+
+plants_json[0..10].map do |plant|
+  Plant.create!(user: user,
+            temp_min: plant["tempmin"]["celsius"],
+            temp_max: plant["tempmax"]["celsius"],
+            ideal_light: plant["ideallight"],
+            tolerated_light: plant["toleratedlight"],
+            common_name: plant["common"][0],
+            latin_name: plant["latin"],
+            family: plant["family"],
+            difficulty_level: rand(0..10),
+            avg_rating: (rand(0..5) * rand(0..10)) / rand(1..10))
+  puts "Created #{plant["common"][0]}"
+end
