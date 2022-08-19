67710e1be2fead378e9ffd93b504665c37c14c7a
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-19 19:20:49 +0100

    TA prep: plants#new and #create

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,14 +9,26 @@ class PlantsController < ApplicationController
   end
 
   def new
+    @plant = Plant.new
   end
 
   def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    if @plant.save
+      redirect_to plant_path(@plant)
+    else
+      render :new
+    end
   end
 
   private
 
   def set_plant
     @plant = Plant.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:common_name, :latin_name, :family, :temp_min, :temp_max, :ideal_light, :tolerated_light, :difficulty_level, :photo)
   end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,7 @@
 class Plant < ApplicationRecord
+  validates :common_name, :latin_name, presence: true
+  validates :difficulty_level, inclusion: { in: (1..10) }
+
   belongs_to :user
   has_one_attached :photo
 end
