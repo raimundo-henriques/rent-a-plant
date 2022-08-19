e94cff603c15b9cf9508c08fcf34391407bd8ca2
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-19 18:49:54 +0100

    TA prep: plants#show

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,11 +1,22 @@
 class PlantsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_plant, only: :show
   def index
     @plants = Plant.all
+  end
+
+  def show
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
   end
 end
